d4d910b3198554901a5db9efcaddf49bf1a926c9
Neil Guertin <neilguertin@gmail.com>
2015-06-23 21:05:34 -0400

    Initial commit

---
+++ Bound in Steel/Bound in Steel/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bound_in_Steel
+{
+    class Board
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
